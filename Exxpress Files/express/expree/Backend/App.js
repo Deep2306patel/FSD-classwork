72e5ee7111266ec81d63bda12932efdba1c00a22
Deep2306patel <103233788+Deep2306patel@users.noreply.github.com>
2025-04-28 13:02:03 +0530

    Update App.js

--- Exxpress Files/express/expree/Backend/App.js
+++ Exxpress Files/express/expree/Backend/App.js
@@ -1,2 +1,8 @@
 app.use(express.static('public',{index:'/login.html'}));
  
+
+app.post('/user', (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    res.send(`Hello, ${firstName} ${lastName}`);
+});
